4f28d9f6650fc8b957a60b84cee147452b5fbf7e
Jayraj Sanas <jayrajsanas175@gmail.com>
2025-08-24 09:16:30 +0530

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
+import Navbar from './components/NavBar';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<BrowserRouter>
+<Navbar/>
+<Routes>
+<Route path="/signup" element={<Signup />} />
+<Route path="/login" element={<Login />} />
+<Route path="/dashboard" element={<Dashboard />} />
+<Route path="/search" element={<Search />} />
+</Routes>
+</BrowserRouter>
+);
 }
+
 
 export default App;
