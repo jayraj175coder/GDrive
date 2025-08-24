28745b2354f3df749fccc70e9aeea9bf6cbfc972
Jayraj Sanas <135996214+jayraj175coder@users.noreply.github.com>
2025-08-24 16:37:42 +0530

    Update ImageCard.jsx

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -6,9 +6,7 @@ export default function ImageCard({ image }) {
     <div className="image-card">
       <div className="image-container">
         <img 
-{/*           src={`http://localhost:5000/${image.filePath}`}  */}
           src={`https://gdrive-4da3.onrender.com/${image.filePath}`} 
-
           alt={image.name} 
           onError={(e) => {
             e.target.src = 'https://via.placeholder.com/150x150?text=Image+Error';
