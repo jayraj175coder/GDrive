fcbfdb5e57ed8372e8121ae7245ec86f4a88aade
Jayraj Sanas <jayrajsanas175@gmail.com>
2025-08-24 14:58:03 +0530

    Prepare for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' 
+});
 
 
 // Add token to requests
